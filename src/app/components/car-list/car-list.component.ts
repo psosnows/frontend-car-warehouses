acf728c1e2d850bcc157d7e300b2a50aba889a6a
Pawel S <psosnows@gmail.com>
2021-04-18 17:05:13 +0200

    preping files for car-list.component

---
+++ src/app/components/car-list/car-list.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-car-list',
+  templateUrl: './components/car-list.component.html',
+  styleUrls: ['./components/car-list.component.css']
+})
+export class CarListComponent implements OnInit {
+  ngOnInit(): void {
+  }
+}
